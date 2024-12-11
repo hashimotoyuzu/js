bce59878285350d9c3768b5fe0e220265fe51422
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-12-11 04:42:18 +0000

    フロントエンド課題02まで

---
+++ 03/3_18.js
@@ -0,0 +1,3 @@
+let inputValue = prompt();  // 入力ダイアログボックスから値を取得
+let x = inputValue * 110 / 100;    // 入力の値に消費税10%を加算して、xに代入
+alert(x);                   // xの値を警告ダイアログボックスに表示
---
+++ 04/4_01.js
@@ -0,0 +1,30 @@
+let mon = 1;
+if (a >= 1 && 12) {
+    console.log('mon'); 
+} else if (a =1) {
+    console.log('January'); 
+} else if (a =2) {
+    console.log('February'); 
+} else if (a =3) {
+    console.log('March'); 
+} else if (a =4) {
+    console.log('April'); 
+} else if (a =5) {
+    console.log('May'); 
+} else if (a =6) {
+    console.log('June'); 
+} else if (a =7) {
+    console.log('July'); 
+} else if (a =8) {
+    console.log('August'); 
+} else if (a =9) {
+    console.log('September'); 
+} else if (a =10) {
+    console.log('October'); 
+} else if (a =11) {
+    console.log('November'); 
+} else if (a =12) {
+    console.log('December'); 
+} else {
+    console.log('不明です');    
+}
