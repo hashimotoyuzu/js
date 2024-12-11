14b2ec9d07e3a1078ecce86d824e997e681e37b6
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-12-11 10:48:19 +0000

    フロントエンド課題06

---
+++ 06/6_01.js
@@ -0,0 +1,10 @@
+// 配列の定義
+let fruit = ['apricot', 'strawberry'];
+// 配列の先頭に追加
+fruit.unshift('pear');
+// 配列の末尾に追加
+fruit.push('grape');
+// 4番目のデータを'lime'に書き換える
+fruit[3] = 'lime'
+//配列fruitをconsole.logで出力
+console.log(fruit);
