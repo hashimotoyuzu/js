af7ce9a4a81187c6f334274d5ad5d11e612ad977
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-12-17 03:05:25 +0000

    フロントエンド課題08

---
+++ 08/8_10.js
@@ -0,0 +1,6 @@
+function area(p1, p2,) {
+    let p = p1 * p2 / 2;        //　引数p1×引数p2÷2
+    return p;               // 結果を戻す
+}
+//　ユーザー定義関数areaの呼び出し
+console.log(area(12, 10,));
---
+++ 08/8_11.js
@@ -0,0 +1,7 @@
+// 名前のない関数の定義（変数に関数を代入）
+var area = function(p1, p2) {
+    let p = p1 * p2 / 2;        //　引数p1×引数p2÷2
+    return p;               // 結果を戻す
+}
+// 関数の実行
+console.log(area(12, 10));
