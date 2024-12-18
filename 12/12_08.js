d1566ec4b1b9a9c819d59a53837c9c23a87f82bb
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-12-18 13:05:48 +0000

    フロントエンド課題12途中経過

---
+++ 12/12_08.js
@@ -0,0 +1,6 @@
+let target = document.getElementById('test-input');
+target.addEventListener('blur', function() {
+    let span = document.getElementById('text01');
+    span.id = 'test-input';
+    
+});
